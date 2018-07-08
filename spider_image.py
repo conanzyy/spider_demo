892ea0502d44c18dbefeb8da53ba3ea04d14d9b2
conanzyy <conanzyy@163.com>
2018-07-08 17:38:11 +0800

    111

--- spider_image.py
+++ spider_image.py
@@ -15,20 +15,21 @@ total = '123' + \
 print ('total:%s' % total)
 
 # url = "https://image.baidu.com/search/index?tn=baiduimage&ipn=r&ct=201326592&cl=2&lm=-1&st=-1&fm=result&fr=&sf=1&fmq=1527833312292_R&pv=&ic=0&nc=1&z=&se=1&showtab=0&fb=0&width=&height=&face=0&istype=2&ie=utf-8&word=%E7%BE%8E%E5%A5%B3"
-url = "https://tieba.baidu.com/f?kw=%E7%BE%8E%E5%A5%B3&ie=utf-8&tab=album"
+# url = "https://tieba.baidu.com/f?kw=%E7%BE%8E%E5%A5%B3&ie=utf-8&tab=album"
+url="http://world.people.com.cn/n1/2018/0625/c1002-30083165.html"
 
 
 #第一种网页下载方法
-html1 = urllib.request.urlopen(url).read().decode('utf-8')
-soup1 = BeautifulSoup(html1, 'html.parser',from_encoding="utf-8")
-print ("长度1:%d" % len(html1))
+# html1 = urllib.request.urlopen(url).read().decode('utf-8')
+# soup1 = BeautifulSoup(html1, 'html.parser',from_encoding="utf-8")
+# print ("长度1:%d" % len(html1))
 
 #第二种网页下载方法
-# request = urllib.request.Request(url)
-# request.add_header("user-agent","Mozilla/5.0") #用户代理，伪装为一个浏览器
-# html2 = urllib.request.urlopen(url).read()
-# soup2 = BeautifulSoup(html2, 'html.parser',from_encoding="utf-8")
-# print ("长度2:%d" % len(html2))
+request = urllib.request.Request(url)
+request.add_header("user-agent","Mozilla/5.0") #用户代理，伪装为一个浏览器
+html2 = urllib.request.urlopen(url).read()
+soup2 = BeautifulSoup(html2, 'html.parser',from_encoding="utf-8")
+print ("长度2:%d" % len(html2))
 
 #第三种网页下载方法
 # cj = http.cookiejar.CookieJar()  #创建一个cookie的容器cj
@@ -42,7 +43,7 @@ print ("长度1:%d" % len(html1))
 
 # 用Beautiful Soup结合正则表达式来提取包含所有图片链接（img标签中，class=**，以.jpg结尾的链接）的语句
 # links = soup1.find('div',class_ = "imgpage")
-links1 = soup1.find_all("img")
+links1 = soup2.find_all("img")
 # links2 = soup2.find_all('img',src = re.compile(r'https://imgsa.baidu.com/forum/'))
 # links3 = soup3.find_all('img',src = re.compile(r'https://imgsa.baidu.com/forum/'))
 
